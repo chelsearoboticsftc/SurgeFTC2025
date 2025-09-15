52004025434ed45502c5b8d496c37a7f510f978a
maclarenbenjamin <maclarenbenjamin@gmail.com>
2025-09-15 19:14:18 -0400

    Update BasicTeleopDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/BasicTeleopDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/BasicTeleopDrive.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
+//trst
 
 @TeleOp
 public class BasicTeleopDrive extends LinearOpMode {
@@ -32,3 +33,4 @@ public class BasicTeleopDrive extends LinearOpMode {
 //Wesley and Mack
 //Are all cool
 // :P
+//aaaahh
