8fa9613762e0e08ee4b28712939bb372fba9f361
Bryan Johnson <d.bryan.johnson@gmail.com>
2025-09-24 10:46:54 -0400

    reset BasicTeleopDrive, remove ControlHubTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/BasicTeleopDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/BasicTeleopDrive.java
@@ -1,13 +1,11 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
-import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 
@@ -15,31 +13,20 @@ import org.firstinspires.ftc.teamcode.MecanumDrive;
 public class BasicTeleopDrive extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        Telemetry tel = FtcDashboard.getInstance().getTelemetry();
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
+
         waitForStart();
-        tel.addData("Foo", "Bar");
-        tel.update();
-        tel.addData("HardwareMap", hardwareMap);
-//        hardwareMap.get
 
-        int i = 0;
-        while(opModeIsActive()) {
-            tel.addData("Count", i);
-            tel.update();
-            i += 1;
+        while(opModeIsActive()){
+            drive.setDrivePowers(
+                new PoseVelocity2d(
+                    new Vector2d(
+                        -gamepad1.left_stick_y,
+                        -gamepad1.left_stick_x
+                    ),
+                    -gamepad1.right_stick_y
+                )
+            );
         }
-
-//        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
-
-//        waitForStart();
-
-//        while(opModeIsActive()){
-//            drive.setDrivePowers(
-//                    new PoseVelocity2d(
-//                            new Vector2d(-gamepad1.left_stick_y,
-//                                         -gamepad1.left_stick_x),
-//                            -gamepad1.left_stick_x));
-//        }
-//    }
     }
 }
