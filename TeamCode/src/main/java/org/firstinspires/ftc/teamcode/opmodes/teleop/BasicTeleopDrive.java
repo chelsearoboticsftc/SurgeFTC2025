5237e536477b9a60e68fe8a1c32d80cbf280078d
surgechelsearobotics <surgechelsearobotics@gmail.com>
2025-10-01 19:01:18 -0400

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/BasicTeleopDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/BasicTeleopDrive.java
@@ -13,6 +13,7 @@ public class BasicTeleopDrive extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
+        Shooter test1 = new Shooter(hardwareMap);
 
         waitForStart();
 
@@ -23,6 +24,8 @@ public class BasicTeleopDrive extends LinearOpMode {
                             new Vector2d(-gamepad1.left_stick_y,
                                          -gamepad1.left_stick_x),
                             -gamepad1.right_stick_x));
+            if(gamepad1.right_trigger > 0){
+                test1.setMotorPower(0.5);
         }
     }
 }
