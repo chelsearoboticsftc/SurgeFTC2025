bd40b09eb5dbdcdfe6e9f6f9e2ed3a1375275a1d
surgechelsearobotics <surgechelsearobotics@gmail.com>
2025-10-06 18:13:51 -0400

    Update BasicTeleopDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/BasicTeleopDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/BasicTeleopDrive.java
@@ -23,13 +23,12 @@ public class BasicTeleopDrive extends LinearOpMode {
 
         waitForStart();
 
-        while(opModeIsActive()) {
-
-            drive.setDrivePowers(
-                    new PoseVelocity2d(
-                            new Vector2d(-gamepad1.left_stick_y,
-                                    -gamepad1.left_stick_x),
-                            -gamepad1.right_stick_x));
+        while(opModeIsActive()){ drive.setDrivePowers(
+                
+                new PoseVelocity2d(
+                        new Vector2d(-gamepad1.left_stick_y,
+                                -gamepad1.left_stick_x),
+                        -gamepad1.right_stick_x));
 
 
            /* if (gamepad2.a) {
