915496ef61418299925fd0285bbc5035264fdab2
maclarenbenjamin <maclarenbenjamin@gmail.com>
2025-10-08 18:02:08 -0400

    Update BasicTeleopDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/BasicTeleopDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/BasicTeleopDrive.java
@@ -6,8 +6,9 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
-import org.firstinspires.ftc.teamcode.subsystems.example.Shooter;
+import org.firstinspires.ftc.teamcode.subsystems.example.SmartShooter;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.example.SmartShooterConstants;
 //trst
 //Greyson is better than Ben at google baseball
 
@@ -18,7 +19,7 @@ public class BasicTeleopDrive extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
-        Shooter test1 = new Shooter(hardwareMap);
+        SmartShooter test1 = new SmartShooter(hardwareMap);
         // servo = hardwareMap.get(Servo.class, "servo3");
 //EVIL SERVO >:O
 
@@ -57,7 +58,8 @@ public class BasicTeleopDrive extends LinearOpMode {
             }
             test1.setMotorPower(bob);
 
-//goofy code: String smth = String.valueOf(98.25);
+/*goofy code: String smth = String.valueOf(98.25);
 //telemetry.addData("idk",String.valueOf(smth));
 //telemetry.update();
-}           */
+
+            */
