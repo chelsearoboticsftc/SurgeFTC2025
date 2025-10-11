37fabc00a41e7528e2d16c7fd1e18b1fa6d3c1f9
surgechelsearobotics <surgechelsearobotics@gmail.com>
2025-10-11 10:24:52 -0400

    Update SurgeTeleopBlue.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/SurgeTeleopBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/SurgeTeleopBlue.java
@@ -6,6 +6,8 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.configuration.typecontainers.ServoConfigurationType;
+
 import org.firstinspires.ftc.teamcode.subsystems.example.Shooter;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 //trst
