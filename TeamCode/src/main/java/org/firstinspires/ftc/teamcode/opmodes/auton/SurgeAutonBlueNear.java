f45ff799f5a710edcad67f64282036d9eb1df53b
surgechelsearobotics <surgechelsearobotics@gmail.com>
2025-10-25 10:40:19 -0400

    made files look the same

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/SurgeAutonBlueNear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/SurgeAutonBlueNear.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.opmodes.auton;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 @Autonomous
 public class SurgeAutonBlueNear extends LinearOpMode {
     @Override
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/SurgeAutonRedNear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/SurgeAutonRedNear.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.opmodes.auton;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-
 @Autonomous
 public class SurgeAutonRedNear extends LinearOpMode {
     @Override
