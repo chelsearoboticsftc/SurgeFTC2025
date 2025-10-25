c2b0767d5591b1e3afd930e9ac870ee4a8540e0c
surgechelsearobotics <surgechelsearobotics@gmail.com>
2025-10-25 10:30:02 -0400

    Update basicAuton.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/basicAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/basicAuton.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode.opmodes.auton;
 
-public class basicAuton {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class basicAuton extends LinearOpMode{
+    @Override
+    public void runOpMode() throws InterruptedException{
+
+    }
+    
 }
