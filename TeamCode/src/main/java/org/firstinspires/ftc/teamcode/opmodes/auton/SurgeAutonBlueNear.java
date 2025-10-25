2234899c0247a5a86ba72e0eb80d552aa680e0bf
DiesingGreyson <gdiesing19@gmail.com>
2025-10-25 11:18:33 -0400

    Updated All Autons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/SurgeAutonBlueNear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/SurgeAutonBlueNear.java
@@ -1,11 +1,27 @@
 package org.firstinspires.ftc.teamcode.opmodes.auton;
 
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
 @Autonomous
 public class SurgeAutonBlueNear extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
+        waitForStart();
 
+        Thread.sleep((2000));
+
+        if (isStopRequested()) return;
+        Actions.runBlocking(
+                drive.actionBuilder(new Pose2d(  0,  0, 0))
+                        .lineToX(120)
+                        .build()
+
+        );
     }
 }
