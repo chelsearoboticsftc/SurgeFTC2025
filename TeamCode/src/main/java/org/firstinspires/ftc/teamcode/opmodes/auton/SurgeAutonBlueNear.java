8ff18c17cec97a182e4dfbeccee2a8f7d9c2bfaf
DiesingGreyson <gdiesing19@gmail.com>
2025-10-25 13:52:55 -0400

    Update SurgeAutonBlueNear.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/SurgeAutonBlueNear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/SurgeAutonBlueNear.java
@@ -27,10 +27,9 @@ public class SurgeAutonBlueNear extends LinearOpMode {
         if (isStopRequested()) return;
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(  0,  0, 0))
-                        .lineToX(12)
-                        .lineToY(12)
+                      //  .lineToX(12)
+                        .strafeTo(10)
                         .build()
-
         );
     }
 }
