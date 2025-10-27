c61d87fcea699f08ef8deab84c61dec0db4903b4
DiesingGreyson <gdiesing19@gmail.com>
2025-10-27 19:13:26 -0400

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/SurgeAutonBlueNear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/SurgeAutonBlueNear.java
@@ -22,7 +22,7 @@ public class SurgeAutonBlueNear extends LinearOpMode {
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(  0,  0, 0))
                         //  .lineToX(12)
-                        .turnTo(0.5)
+                        .turnTo(0.1)
                         .lineToX(-12)
                         .build()
         );
