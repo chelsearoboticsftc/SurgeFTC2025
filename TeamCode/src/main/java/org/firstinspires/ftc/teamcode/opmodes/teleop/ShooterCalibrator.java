4de00e38ba8d6895df89488a08c117f28c39c7d5
maclarenbenjamin <maclarenbenjamin@gmail.com>
2025-11-05 18:58:45 -0500

    hello

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/ShooterCalibrator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/ShooterCalibrator.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.subsystems.example.SmartShooter;
 
+
 @TeleOp
 public class ShooterCalibrator extends LinearOpMode {
     @Override
