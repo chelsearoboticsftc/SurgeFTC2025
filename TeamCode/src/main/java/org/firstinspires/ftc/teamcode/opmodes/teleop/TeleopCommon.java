d9237e49871ca1fe8ec8bf97c9ff9cc2aa89c906
surgechelsearobotics <surgechelsearobotics@gmail.com>
2025-11-05 19:24:21 -0500

    Update TeleopCommon.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TeleopCommon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TeleopCommon.java
@@ -35,7 +35,7 @@ public class TeleopCommon extends LinearOpMode {
 
                     new PoseVelocity2d(
                             new Vector2d(gamepad1.left_stick_y,
-                                    gamepad1.left_stick_x),
+                                    -gamepad2.left_stick_x),
                             -gamepad1.right_stick_x));
 
             if(gamepad2.rightBumperWasPressed()){
